f3e28a2507675b2184670d0925a43b006bf95b0d
Lukas Stuart-Fry <lstuartfry@gmail.com>
2024-05-07 20:16:48 -0700

    adds prettier, tailwind classnames prettier plugin, modifies tailwind classname order in app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <main className="flex min-h-screen h-screen items-center justify-center bg-darkerGray text-white">
-      <div className="bg-red-50 rounded-md w-1/3 h-2/3 min-w-[300px] flex flex-col overflow-hidden">
+    <main className="flex h-screen min-h-screen items-center justify-center bg-darkerGray text-white">
+      <div className="flex h-2/3 w-1/3 min-w-[300px] flex-col overflow-hidden rounded-md bg-red-50">
         <div className="bg-darkGray">timer</div>
         <div>clock</div>
         <div>footer</div>
