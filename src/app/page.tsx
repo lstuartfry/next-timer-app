658249993f53bee12100d0d2ce6ca1e0d85dc2a1
Lukas Stuart-Fry <lstuartfry@gmail.com>
2024-05-08 13:32:57 -0700

    adds ability to manually set the timer via keyboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,32 @@
 "use client";
 
 import Timer from "@/components/Timer";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export default function App() {
-  // default the timer to 60 seconds.
   const time = new Date();
+
+  // default the timer to 60 seconds.
   time.setSeconds(time.getSeconds() + 60);
 
   const [timer, setTimer] = useState<Date>(time);
+
+  const handleChange = useCallback(
+    ({ minutes, seconds }: { minutes: number; seconds: number }) => {
+      const newTime = new Date();
+      newTime.setMinutes(newTime.getMinutes() + minutes);
+      newTime.setSeconds(newTime.getSeconds() + seconds);
+      setTimer(newTime);
+    },
+    [],
+  );
 
   return (
     <main className="flex h-screen min-h-screen items-center justify-center bg-dark-gray-800 font-roboto text-white">
       <div className="flex h-2/3 w-1/3 min-w-[300px] flex-col overflow-hidden rounded-md bg-red-50">
         <div className="flex justify-center bg-dark-gray-700 py-6">Timer</div>
         <div className="flex flex-1 items-center justify-center bg-dark-gray-500">
-          <Timer expiryTimestamp={timer} />
+          <Timer expiryTimestamp={timer} onChange={handleChange} />
         </div>
         <div className="flex justify-center bg-dark-gray-500 px-4 py-6">
           footer
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,20 +1,45 @@
 "use client";
 
+import { useEffect } from "react";
 import { useTimer } from "react-timer-hook";
 import InputMask from "react-input-mask";
 
-export default function Timer({ expiryTimestamp }: { expiryTimestamp: Date }) {
-  const { seconds, minutes } = useTimer({
+interface Props {
+  expiryTimestamp: Date;
+  onChange: ({
+    minutes,
+    seconds,
+  }: {
+    minutes: number;
+    seconds: number;
+  }) => void;
+}
+
+export default function Timer({ expiryTimestamp, onChange }: Props) {
+  const { seconds, minutes, pause, restart } = useTimer({
     expiryTimestamp,
     autoStart: false,
   });
 
+  // reset the timer every time the expiryTimestamp is updated
+  useEffect(() => {
+    restart(expiryTimestamp, false);
+  }, [expiryTimestamp, restart]);
+
+  // parse the masked input prior to invoking the 'onChange' callback
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const [minutes, seconds] = e.target.value.split(":");
+    onChange({ minutes: Number(minutes), seconds: Number(seconds) });
+  };
+
+  // format the value to match the desired input mask
   const formattedValue = `${minutes > 9 ? minutes : `0${minutes}`}${seconds > 9 ? seconds : `0${seconds}`}`;
 
   return (
     <InputMask
-      className="bg-transparent text-center"
+      className="bg-transparent text-center text-4xl"
       mask="99:99"
+      onChange={handleChange}
       value={formattedValue}
     />
   );
